221e4fdd75e8f3afaa9f25fe5891d4432a5020df
Gabutops <89936664+Gabutops@users.noreply.github.com>
2023-03-02 21:56:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { Dropdown } from "./dropdowns/dropdown-3/Dropdown";
 
+import '@splidejs/react-splide/css';
 function App() {
+  const options = {
+    type: 'slide',
+    perPage: 2,
+    gap: '1rem',
+    rewind: true,
+    autoplay: true,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Splide options={options}>
+        <SplideSlide>
+          <img src="https://placekitten.com/800/600" alt="Kitten 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="https://placekitten.com/801/600" alt="Kitten 2" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="https://placekitten.com/802/600" alt="Kitten 3" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="https://placekitten.com/803/600" alt="Kitten 4" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src="https://placekitten.com/804/600" alt="Kitten 5" />
+        </SplideSlide>
+      </Splide>
     </div>
   );
 }
